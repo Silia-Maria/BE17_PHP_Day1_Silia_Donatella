38acda9f4aaf4776380ed275ebeb27b41ea35353
Silia Cronauer <silia.maria@web.de>
2022-11-07 11:01:46 +0100

    finished basic exercise

---
+++ Silia/Basic/index.php
@@ -0,0 +1,26 @@
+<?php
+$name = "Silia";
+$age = 26;
+$job = "Manager";
+
+$players = ["Mark", "Greg", "Lisa"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"
+    <title></title>
+</head>
+<body>
+
+<h1 class="heading"> My name is <?php echo $name; ?> and I'm <?php echo $age; ?> years old, and I work as <?php echo $job; ?>  </h1>
+
+<p>The third Player is <?php echo $players[2]; ?> </p>
+
+    
+</body>
+</html>
